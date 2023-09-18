8ce85437a7450ee34e9b6aa3d61b4c1622bea72c
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-18 14:07:00 +0545

    updated the app

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = +process.env.PORT || 3000;
-const indexRouter = require("./routes");
+const PORT = process.env.PORT || 3000; // Use parseInt() if you want to parse it as an integer
+const DB_URL = process.env.DB_URL; // Store the DB URL in a variable
 
-mongoose.connect(process.env.DB_URL).then(() => {
-  console.log("Database connected...");
-});
+mongoose
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database connected...");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
 
 app.use(express.json());
 
+const indexRouter = require("./routes"); // Import your routes at this point
+
 app.use("/", indexRouter);
 
-app.use((err, res, req, next) => {
+app.use((err, req, res, next) => {
+  // Correct the order of parameters in this middleware function
   err = err.toString() || "Something went wrong...";
   res.status(500).json({ data: "", msg: err });
 });
--- modules/todos/todo.api.js
+++ modules/todos/todo.api.js
@@ -10,22 +10,34 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/:id", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  // Removed ":id" from the route
   try {
     const { title } = req.body;
     if (!title) throw new Error("Title is missing..");
     const result = await Controller.create({ title });
+    res.status(201).json({ data: result, msg: "success" }); // Use 201 Created status for successful creation
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { status, title } = req.body;
+    const result = await Controller.updateResource(id, { status, title });
     res.json({ data: result, msg: "success" });
   } catch (e) {
     next(e);
   }
 });
 
-router.patch("/", async (req, res, next) => {
+router.patch("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const { status } = req.body;
-    const result = await Controller.updateStatus(id, { satus });
+    const result = await Controller.updateStatus(id, { status });
     res.json({ data: result, msg: "success" });
   } catch (e) {
     next(e);
